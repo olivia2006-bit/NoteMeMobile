7b67eec0bab4111f814bd83113992971a5cef5e9
olivia2006-bit <abuiolive@gmail.com>
2025-06-20 10:39:19 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'NoteMeMobile',
+  webDir: 'www'
+};
+
+export default config;
